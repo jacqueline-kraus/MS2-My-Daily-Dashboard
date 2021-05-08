fe7043921505861f6bfd81ada316e47c11974f36
Jacqueline Kraus <jacquelinekraus1992@gmail.com>
2021-05-08 22:02:43 +0200

    created function to get ingredients and write the ingredients list to index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,48 @@
 $(document).ready(function(){
 
     let randomMeal = fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+        // .then(response => {
+        //     response.json().then(jsonObject => {   
+        //         const arrayIngredients = [
+        //             jsonObject.meals[0].strIngredient1,
+        //             jsonObject.meals[0].strIngredient2,
+        //             jsonObject.meals[0].strIngredient3,
+        //         ];
+        //         createIngredientsList(arrayIngredients);
+        //     });
+        // })
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(jsonObject => [
+            jsonObject.meals[0].strMeasure1 + jsonObject.meals[0].strIngredient1,
+            jsonObject.meals[0].strMeasure2 + jsonObject.meals[0].strIngredient2,
+            jsonObject.meals[0].strMeasure3 + jsonObject.meals[0].strIngredient,
+        ])
+        .then(ingredients => createIngredientsList(ingredients))
+    
 });
+
+function createIngredientsList(ingredientsListItems) {
+    //let ingredientsListItems = [{
+      //  strIngredient1: '',
+        //strIngredient2: '',
+
+
+   // }];
+    // https://getbutterfly.com/generate-html-list-from-javascript-array/
+    // Make the list
+    let listElement = document.createElement('ul');
+
+    // Add it to the page
+    document.getElementById('mealIngredientsList').appendChild(listElement);
+
+    for (i = 0; i < ingredientsListItems.length; ++i) {
+        // create an item for each one
+        let listItem = document.createElement('li');
+
+        // Add the item text
+        listItem.innerHTML = ingredientsListItems[i];
+
+        // Add listItem to the listElement
+        listElement.appendChild(listItem);
+};
+}
